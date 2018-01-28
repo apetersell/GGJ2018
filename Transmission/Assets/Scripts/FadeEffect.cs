49b165b49cce2d6b5bc9aa01a1a47a4a43e5af2d
Unknown <andrewpetersell@gmail.com>
2018-01-28 13:05:37 -0500

    First Start Screen Working

---
+++ Transmission/Assets/Scripts/FadeEffect.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeEffect : MonoBehaviour {
+
+
+	public float fadeDuration;
+	public int nextScene;
+	public bool fadeInAtStart;
+	Color displayColor;
+	Image img;
+
+	// Use this for initialization
+	void Start () 
+	{
+		img = GetComponent<Image> ();
+		if (fadeInAtStart) 
+		{
+			StartCoroutine (fadeIn());
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		img.color = displayColor;
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			StartCoroutine (fadeOut());
+		}
+	}
+
+	public IEnumerator fadeIn ()
+	{
+		float currentTime = 0;
+		while (currentTime < fadeDuration) 
+		{
+			displayColor = Color.Lerp (Color.black, Color.clear, currentTime / fadeDuration);
+			currentTime += Time.deltaTime;
+			yield return null;
+		}
+	}
+
+	public IEnumerator fadeOut ()
+	{
+		float currentTime = 0;
+		while (currentTime < fadeDuration) 
+		{
+			displayColor = Color.Lerp (Color.clear, Color.black, currentTime / fadeDuration);
+			currentTime += Time.deltaTime;
+			yield return null;
+		}
+
+		SceneManager.LoadScene (nextScene);
+	}
+}
