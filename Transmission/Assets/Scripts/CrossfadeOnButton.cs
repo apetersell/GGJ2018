d5f46339a26d3ca52d81641332c78dcd609e3a33
robrappoport <rjr363@nyu.edu>
2018-01-27 19:37:07 -0500

    audioManager in

---
+++ Transmission/Assets/Scripts/CrossfadeOnButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrossfadeOnButton : MonoBehaviour
+{
+    public AudioClip[] tracks;
+
+    public KeyCode space = KeyCode.Space;
+    public float fadeTime = 1.0f;
+
+    private int currentTrack = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(space))
+        {
+            currentTrack++;
+            if (currentTrack >= tracks.Length)
+            {
+                currentTrack = 0;
+            }
+            AudioManager.Crossfade(tracks[currentTrack], fadeTime);
+        }
+    }
+}
