162c78b5ddda4ff1a5faaccf24110b7b457188fc
robrappoport <rjr363@nyu.edu>
2018-01-28 12:35:09 -0500

    scenes in SFX in music in

---
+++ Transmission/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("TitleScreen");
+        }
+    }
+    public void ButtonClick ()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
